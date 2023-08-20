76105688de38a731997e8c8b0fa282e2e5ee173f
VV1nc3nt <gormmyz@gmail.com>
2023-08-20 23:20:14 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
 /* eslint-disable no-unused-vars */
 /* eslint-disable semi */
 import './js/app'
 import './css/style.css';
-import img from '../assets/img/goblin.png'
+import img from './assets/img/goblin.png'
